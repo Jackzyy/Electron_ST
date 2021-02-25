c67c4666fe3485ff312692251e9ef6c97ee49ac6
Zyy <Zyy@email.com>
2021-02-25 17:58:02 +0800

    update code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,43 @@
 <template>
-  <div>Home</div>
+  <div>COUNT:{{ number }}</div>
+  <div>{{ computedNum }}</div>
+  <el-button @click="add">增加</el-button>
+  <el-button @click="reset">重置</el-button>
 </template>
 
 <script>
+import { reactive, toRefs, watch, computed, onMounted } from 'vue'
 export default {
   setup() {
-    return {}
+    let obj = reactive({ number: 0 })
+    function add() {
+      obj.number++
+    }
+    function reset() {
+      obj.number = 0
+    }
+
+    const computedNum = computed(() => {
+      return obj.number + 1000
+    })
+
+    watch(
+      () => obj.number,
+      (newValue, oldValue) => {
+        console.log(newValue, oldValue)
+      }
+    )
+
+    onMounted(() => {
+      console.log(222)
+    })
+
+    return {
+      ...toRefs(obj),
+      computedNum,
+      add,
+      reset
+    }
   }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -5,22 +5,22 @@ module.exports = {
       builderOptions: {
         appId: 'com.zyy.app',
         productName: 'Zyy', //项目名，也是生成的安装文件名，即aDemo.exe
-        copyright: `Copyright © ${new Date().getFullYear()}`, //版权信息
+        copyright: `Copyright © ${new Date().getFullYear()}` //版权信息
         //win相关配置
         // win: {
         //   icon: './src/assets/logo.png' //图标，当前图标在根目录下，注意这里有两个坑
         // },
-        nsis: {
-          oneClick: false, // 是否一键安装
-          allowToChangeInstallationDirectory: true // 允许修改安装目录
-        },
+        // nsis: {
+        //   oneClick: false, // 是否一键安装
+        //   allowToChangeInstallationDirectory: true // 允许修改安装目录
+        // }
         // 复制文件到指定目录
-        extraResources: [
-          {
-            from: '',
-            to: ''
-          }
-        ]
+        // extraResources: [
+        //   {
+        //     from: '',
+        //     to: ''
+        //   }
+        // ]
       }
     }
   }
